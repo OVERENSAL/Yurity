f57e6b29eea68784b5ab4d9e1a0f4fdb5e3844ac
Vladislav Sadovin <sadovin.vladislav@gmail.com>
2021-12-19 15:10:53 +0300

    Some refactoring and deleted delete big archive from commit

---
+++ Constructor/Assets/ChooseModeButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseModeButton : MonoBehaviour
+{
+    public GameObject startLayout;
+    public GameObject chooseMapLayout;
+    public GameObject gameMapLayout;
+    public GameObject constructorMapLayout;
+    public GameObject gameCanvas;
+
+    void Start()
+    {
+        startLayout.SetActive(true);
+        chooseMapLayout.SetActive(false);
+    }
+
+    public void onClickConstructor()
+    {
+        startLayout.SetActive(false);
+        chooseMapLayout.SetActive(true);
+        constructorMapLayout.SetActive(true);
+        gameMapLayout.SetActive(false);
+    }
+
+    public void onClickGame()
+    {
+        startLayout.SetActive(false);
+        chooseMapLayout.SetActive(false);
+        constructorMapLayout.SetActive(false);
+        gameMapLayout.SetActive(true);
+        gameCanvas.SetActive(true);
+    }
+}
---
+++ Constructor/Assets/GameProcess.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProcess : MonoBehaviour
+{
+    public GameObject playerModel;
+    public GameObject currentCanvas;
+    public GameObject menuCanvas;
+    public Transform cells;
+    public float speed = 3f;
+
+    private List<GameObject> mapCells = new List<GameObject>();
+    private int stepsCount = 0;
+    private Vector3 destination = new Vector3(-0.6f, 0.5f, -2.4f);
+    private bool isGo = false;
+    private bool stepComplete = false;
+    private int steps = 0;
+
+
+    public void GetRandom()
+    {
+        steps = Random.Range(1, 5);
+        stepsCount += steps;
+        if (stepsCount < cells.childCount)
+        {
+            Debug.Log(steps);
+            destination = new Vector3(mapCells[stepsCount - steps].transform.position.x, playerModel.transform.position.y, mapCells[stepsCount - steps].transform.position.z);
+            isGo = true;
+            stepComplete = false;
+
+            Debug.Log("Your effect: " + mapCells[stepsCount].gameObject.GetComponent<Renderer>().material);
+        }
+        else
+        {
+            stepComplete = true;
+            destination = new Vector3(mapCells[cells.childCount - 1].transform.position.x, playerModel.transform.position.y, mapCells[cells.childCount - 1].transform.position.z);
+            Debug.Log("You win!");
+        }
+    }
+
+    public void getNextStep()
+    {
+        if ((steps > 0) && (stepsCount - steps < cells.childCount))
+        {
+            Debug.Log("Curren cell: " + (stepsCount - steps));
+            steps--;
+            destination = new Vector3(mapCells[stepsCount - steps].transform.position.x, playerModel.transform.position.y, mapCells[stepsCount - steps].transform.position.z);
+            Debug.Log("Curren cell coords: " + destination);
+        }
+        else
+        {
+            stepComplete = true;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCanvas.SetActive(false);
+        menuCanvas.SetActive(true);
+        Debug.Log("START!");
+        foreach (Transform cell in cells)
+        {
+            mapCells.Add(cell.gameObject);
+        }
+        Debug.Log(playerModel.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGo)
+        {
+            if (!stepComplete)
+            {
+                playerModel.transform.position = Vector3.MoveTowards(playerModel.transform.position, destination, Time.deltaTime * speed);
+                if (playerModel.transform.position == destination)
+                {
+                    getNextStep();
+                }
+            }
+        }
+    }
+}
